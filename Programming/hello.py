a7041e0b04836284268ac01f6205c36e593b8701
Spencer Snow <spencer.snow40@gmail.com>
2025-03-24 16:22:59 -0600

    Update of SessionZero.txt/Creation of SessionOne

--- Programming/hello.py
+++ Programming/hello.py
@@ -6,3 +6,13 @@ print(msg)
 
 print(np.random.randint(1,9))
 
+# Assigning to a variable
+current_hour = 7
+print("The current hour is", current_hour)
+
+# Equality operation
+print(current_hour == 12)
+
+# Assigning the result of an equality operation
+is_midnight = current_hour == 12
+print("Is it midnight?", is_midnight)
